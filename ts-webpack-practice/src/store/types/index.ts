241edc8263447d41107e634cad62bfb8b72add44
huangyichao0916 <939121836@qq.com>
2020-09-19 20:37:34 +0800

    study

--- ts-webpack-practice/src/store/types/index.ts
+++ ts-webpack-practice/src/store/types/index.ts
@@ -1,15 +1,5 @@
-/**
- * 本项目使用了combineReducers
- * 这个文件声明了各个子ruducer的state的类型
- * 子reducer有3个，分别是：余额，首页的数据，充值以及购买记录
- */
-
-
-//余额
 export type BalanceState = number;
 
-
-//首页数据(数组)
 interface Data {
     name: string;
     price: number;
@@ -17,19 +7,14 @@ interface Data {
 }
 export type DataState = Array<Data>;
 
-
-//充值以及购买记录(数组)
 interface RecordItem{
-    price?: number;
-    name?: string;
+    price: number;
+    name: string;
     date: string;
-    account?: number;
 }
 export type RecordState  = Array<RecordItem>;
 
 
-
-//根reducer的state的类型
 export type RootState = {
     balance: BalanceState;
     data: DataState;
